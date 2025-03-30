964ce4e8fc0a040c8c4b1532381bd75fc9266f91
Databazator <bleskaceq9@gmail.com>
2025-03-30 11:08:54 +0200

    plane tilt & bird sprite update

--- Assets/Scripts/AirplaneController.cs
+++ Assets/Scripts/AirplaneController.cs
@@ -13,6 +13,10 @@ public class AirplaneController : MonoBehaviour
 
     const float MAX_DESIRED_ROTATION_CHANGE = 120f;
     float _desiredRotationChange = 0f;
+    float _tilt;
+
+    [SerializeField] float _tiltRate = 1f;
+    [SerializeField] float _maxTiltAngle = 30f;
 
     Rigidbody2D _rb;
 
@@ -65,27 +69,60 @@ public class AirplaneController : MonoBehaviour
 
 
     [SerializeField] Transform _airplaneModel;
+    [SerializeField] Transform _airplaneTiltNode;
     [SerializeField] Vector3 modelUp;
     [SerializeField] Vector3 _modelShift;
+    [SerializeField] List<TrailRenderer> _wingTipTrails;
+    [SerializeField] float _wingTrailEmissionThreshold = 0.35f;
     void UpdateModel()
     {
         if (_airplaneModel.IsNil()) return;
         float desiredRotation = Mathf.Atan2(_rb.velocity.y, _rb.velocity.x) * Mathf.Rad2Deg;
         //if(Mathf.Abs(desiredRotation - _airplaneModel.rotation.eulerAngles.z) < 90f)
-            _airplaneModel.rotation = Quaternion.Euler(0, 0, desiredRotation);
+        _airplaneModel.rotation = Quaternion.Euler(0f, 0f, desiredRotation);
 
+        //tilt
+        _airplaneTiltNode.localRotation = Quaternion.Euler(_tilt * _maxTiltAngle, 0f, 0f);
+
+        // set wing trails to emit
+        if(Mathf.Abs(_tilt) >= _wingTrailEmissionThreshold)
+        {
+            foreach (TrailRenderer rend in _wingTipTrails)
+                rend.emitting = true;
+        }
+        else // stop emission
+        {
+            foreach (TrailRenderer rend in _wingTipTrails)
+                rend.emitting = false;
+        }
     }
 
 
     void ReadInput()
     {
+        bool tiltChanged = false;
+
         if (Input.GetKey(KeyCode.A))
+        {
+            _tilt = Mathf.Clamp(_tilt + _tiltRate * Time.deltaTime, -1f, 1f);
+            tiltChanged = true;
             rotationInput += cfg.RotationChangePerSeconds_degrees * Time.deltaTime;
+        }
         if (Input.GetKey(KeyCode.D))
+        {
+            _tilt = Mathf.Clamp(_tilt - _tiltRate * Time.deltaTime, -1f, 1f);
+            tiltChanged = true;
             rotationInput -= cfg.RotationChangePerSeconds_degrees * Time.deltaTime;
-        if (Input.GetKey(KeyCode.W))
-            speedInput += cfg.VelocityChangePerSecond * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))        
+            speedInput += cfg.VelocityChangePerSecond * Time.deltaTime;        
         if (Input.GetKey(KeyCode.S))
             speedInput -= cfg.VelocityChangePerSecond * Time.deltaTime;
+
+        if(!tiltChanged)
+        {
+            float tiltChange = Mathf.Min(Mathf.Abs(_tilt), _tiltRate * Time.deltaTime);
+            _tilt -= _tilt > 0f ?  tiltChange: -tiltChange;
+        }
     }
 }
